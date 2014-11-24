cd7aeaaa7c6a77fe7de00f7d91d0bbc35b7f1732
Brandon Curry <bccurry@memphis.edu>
2014-11-24 07:24:29 -0600

    Updated Abundance
    Updated Abundance Program

--- metasim/abundance.py
+++ metasim/abundance.py
@@ -7,9 +7,15 @@ count2 = 1
 count3 = 1
 count4 = 1
 count5 = 1
+countSum2 = 0
+countSum3 = 0
+countSum4 = 0
+countSum5 = 0
 
 numOfFastaFiles = int(sys.argv[1])
 
+wordCount = []
+totalWordCount = 0
 directory = str(sys.argv[numOfFastaFiles + 2]).split('.')
 
 if os.path.isdir(str(directory[0])) == False:
@@ -17,6 +23,8 @@ if os.path.isdir(str(directory[0])) == False:
 
 for x in range(2,numOfFastaFiles + 2):
     file = open(str(sys.argv[x]),'r')
+    temp, temp2 = str(os.popen("wc -m " + str(sys.argv[x])).read()).split()
+    wordCount.append(int(temp))
     
     if x == 2:
         fileOutput = open(str(sys.argv[numOfFastaFiles + 2]),'w')
@@ -30,6 +38,16 @@ for x in range(2,numOfFastaFiles + 2):
         fileOutput3 = open('3-' + str(sys.argv[numOfFastaFiles + 2]),'a')
         fileOutput4 = open('4-' + str(sys.argv[numOfFastaFiles + 2]),'a')
         fileOutput5 = open('5-' + str(sys.argv[numOfFastaFiles + 2]),'a')
+        count2 = count2 + 1
+        count3 = count3 * 2
+        count4 = count4 * 3
+        count5 = count5 * 4
+        countSum2 = countSum2 + count2
+        countSum3 = countSum3 + count3
+        countSum4 = countSum4 + count4
+        countSum5 = countSum5 + count5
+
+
 
     for line in file:
         if line[0] == '>':
@@ -38,10 +56,7 @@ for x in range(2,numOfFastaFiles + 2):
             fileOutput3.write(str(count3) + ' name "' + line[1:].strip() + '"\n')
             fileOutput4.write(str(count4) + ' name "' + line[1:].strip() + '"\n')
             fileOutput5.write(str(count5) + ' name "' + line[1:].strip() + '"\n')
-            count2 = count2 + 1
-            count3 = count3 * 2
-            count4 = count4 * 3
-            count5 = count5 * 4
+
 
     file.close()
     fileOutput.close()
@@ -50,16 +65,24 @@ for x in range(2,numOfFastaFiles + 2):
     fileOutput4.close()
     fileOutput5.close()
 
+for z in range(0,len(wordCount)):
+    totalWordCount =  totalWordCount + wordCount[z]
+countSum1 = totalWordCount
+countSum2 = countSum2 + totalWordCount
+countSum3 = countSum3 + totalWordCount
+countSum4 = countSum4 + totalWordCount
+countSum5 = countSum5 + totalWordCount
+
 # runs the Metasim program with each taxon profile
 
 for y in range(2,numOfFastaFiles + 2):
     os.system('./Metasim cmd --add-files ' + str(sys.argv[y]))
 
-os.system('./Metasim cmd -d ' + directory[0] + ' --454 -r5000 ' + str(sys.argv[numOfFastaFiles + 2]))
-os.system('./Metasim cmd -d ' + directory[0] + ' --454 -r5000 2-' + str(sys.argv[numOfFastaFiles + 2]))
-os.system('./Metasim cmd -d ' + directory[0] + ' --454 -r5000 3-' + str(sys.argv[numOfFastaFiles + 2]))
-os.system('./Metasim cmd -d ' + directory[0] + ' --454 -r5000 4-' + str(sys.argv[numOfFastaFiles + 2]))
-os.system('./Metasim cmd -d ' + directory[0] + ' --454 -r5000 5-' + str(sys.argv[numOfFastaFiles + 2]))
+os.system('./Metasim cmd -d ' + directory[0] + ' --454 -r ' + str(countSum1) + ' -f 100 -t 0 ' + str(sys.argv[numOfFastaFiles + 2]))
+os.system('./Metasim cmd -d ' + directory[0] + ' --454 -r ' + str(countSum2) + ' -f 100 -t 0 2-' + str(sys.argv[numOfFastaFiles + 2]))
+os.system('./Metasim cmd -d ' + directory[0] + ' --454 -r ' + str(countSum3) + ' -f 100 -t 0 3-' + str(sys.argv[numOfFastaFiles + 2]))
+os.system('./Metasim cmd -d ' + directory[0] + ' --454 -r ' + str(countSum4) + ' -f 100 -t 0 4-' + str(sys.argv[numOfFastaFiles + 2]))
+os.system('./Metasim cmd -d ' + directory[0] + ' --454 -r ' + str(countSum5) + ' -f 100 -t 0 5-' + str(sys.argv[numOfFastaFiles + 2]))
 
 # delete the taxon profiles after simulation
 
