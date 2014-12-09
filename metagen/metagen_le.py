5b877e992b0f23484d2c09a6486fdc63858e9a9c
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2014-12-09 12:34:31 -0600

    Update metagen_le.py

--- metagen/metagen_le.py
+++ metagen/metagen_le.py
@@ -11,6 +11,7 @@ f = open(infile, "r")
 
 F = {}
 b = []
+name = []
 
 # n is number of k-mers
 s1 = datetime.now()
@@ -20,10 +21,14 @@ for line in f.readlines():
     #print(line)
     part = line.split(",")
     #print part
-    b.append(float(part[len(part)-1]))
-    for j in range(1,len(part)-1):
-    	F[n,j-1] = float(part[j])
-    n = n + 1
+    if (part[0] != "K-mer"):
+        b.append(float(part[len(part)-1]))
+        for j in range(1,len(part)-1):
+        	F[n,j-1] = float(part[j])
+        n = n + 1
+    else:
+        for idx in range(1,len(part)-1):
+            name.append(part[idx])
 
 # g is number of genomes
 g = len(part) - 2
@@ -39,11 +44,18 @@ try:
 	# Variables
 	x = [None] * (g)
 	for i in range(g):
-		x[i] = m.addVar(lb = 0, vtype = GRB.CONTINUOUS, name="x"+str(i))	
+		x[i] = m.addVar(lb = 0, vtype = GRB.CONTINUOUS, name=name[i])	
 			
 	m.update()
 
-	# Equations
+	"""# Objective
+	obj = LinExpr()	
+	for i in range(g):
+		obj += y[i]
+	
+	m.setObjective(obj, GRB.MINIMIZE)"""
+
+	# Constraints
 	for i in range(n):
 		constr = LinExpr()
 		for j in range(g):
@@ -57,33 +69,31 @@ try:
 	s5 = datetime.now()
         print "\n"
 	m.optimize()
-     
 
-    if m.SolCount > 0:
-        for v in m.getVars():
-			print('%s %f' %(v.varName, v.x))
-        print ('Obj: %g' %m.objVal)
-    else:
-        print "No solution found."
+        if m.SolCount > 0:
+            for v in m.getVars():
+		print('%s %f' %(v.varName, v.x))
 
-    s6 = datetime.now()
-
-	if m.SolCount > 0:
-	    out = open(outfile,'w')
-	    for v in m.getVars():    	
-	        out.write('%s, %f \n' %(v.varName, float(v.x)))
-	    out.close()
-
-	log = open(logfile,'w')
-	log.write("Number of genomes = %d\n" %g)
-	log.write("Number of k-mers = %d\n" %n)
-	log.write("Read input = " + str(s2 - s1) + "\n")
-	log.write("Build model = " + str(s4 - s3) + "\n")
-	log.write("Solve LP = " + str(s4 - s3))
-	log.close()
+            print ('Obj: %g' %m.objVal)
+        else:
+            print "No solution found."
 	
 except GurobiError as e:
 	print('Error reported')
         print e.errno
 
+s6 = datetime.now()
 
+if m.SolCount > 0:
+    out = open(outfile,'w')
+    for v in m.getVars():    	
+        out.write('%s, %f \n' %(v.varName, float(v.x)))
+    out.close()
+
+log = open(logfile,'w')
+log.write("Number of genomes = %d\n" %g)
+log.write("Number of k-mers = %d\n" %n)
+log.write("Read input = " + str(s2 - s1) + "\n")
+log.write("Build model = " + str(s4 - s3) + "\n")
+log.write("Solve LP = " + str(s4 - s3))
+log.close()
